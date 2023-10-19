114afd4c90212160b421895c83d0883d3dbf7279
unknown <lucasfrz26@gmail.com>
2023-10-19 13:36:55 -0300

    Instalando react router dom

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme(){
+    return(
+        <div>
+            <h1>PAGINA OS DETALHES DOS FILMES</h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>HOME DESSA APP</h1>
+        </div>
+    )
+}
+
+export default Home;
