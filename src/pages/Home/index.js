cbce3c9916e4b2c21e9131792b4348a06c0fd283
unknown <lucasfrz26@gmail.com>
2023-11-18 19:28:05 -0300

    ajeitando e listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,47 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+// URL DA API: /movie/now_playing?api_key=6b450b092e45611e0075def3bab2ca1b&language=pt-BR
+
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "6b450b092e45611e0075def3bab2ca1b",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            //console.log(response);
+            setFilmes(response.data.results.slice(0,10))
+
+        }
+
+        loadFilmes();
+
+    },[])
+
     return(
-        <div>
-            <h1>HOME DESSA APP</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=>{
+                    return(
+                        <article>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,11 @@
+import axios from 'axios'
 
 // BASE DA URL: https://api.themoviedb.org/3
 // URL DA API: /movie/550?api_key=6b450b092e45611e0075def3bab2ca1b&language=pt-BR
+
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3'
+});
+
+export default api;
