de30ad9e1a3ecf313249f4b08594165553cd07e4
unknown <lucasfrz26@gmail.com>
2023-11-20 14:54:50 -0300

    ajeitando pagina detalhes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import './filme-info.css';
 import api from '../../services/api';
+
 
 function Filme(){
     const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         async function loadFilme(){
@@ -15,7 +18,8 @@ function Filme(){
                 }
             })
             .then((response)=>{
-                console.log(response);
+                setFilme(response.data);
+                setLoading(false);
             })
             .catch(()=>{
                 console.log("NAO ACHO NAO VIU, BURRO")
@@ -24,12 +28,41 @@ function Filme(){
 
 
         loadFilme();
+
+        return () => {
+            console.log("componente foi desmontado ")
+        }
+
+
+
     }, [])
 
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
 
     return(
-        <div>
-            <h1>Acessando FILME {id} </h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} /10</strong>
+
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button>
+                    <a href="#" >
+                        Trailer
+                    </a>
+                </button>
+            </div>
+
         </div>
     )
 }
