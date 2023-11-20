739d5df52cdc6a0a57cf163d291fc81aa28d53b4
unknown <lucasfrz26@gmail.com>
2023-11-20 14:03:11 -0300

    ajeitando pagina detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from '../../services/api';
+
 function Filme(){
+    const { id } = useParams();
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: "6b450b092e45611e0075def3bab2ca1b",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                console.log(response);
+            })
+            .catch(()=>{
+                console.log("NAO ACHO NAO VIU, BURRO")
+            })
+        }
+
+
+        loadFilme();
+    }, [])
+
+
     return(
         <div>
-            <h1>PAGINA OS DETALHES DOS FILMES</h1>
+            <h1>Acessando FILME {id} </h1>
         </div>
     )
 }
